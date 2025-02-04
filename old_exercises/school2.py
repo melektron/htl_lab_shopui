46193698b38b4ac30606b2d6f9a8532f92eae2d4
melektron <matteo@elektron.work>
2025-02-04 14:37:50 +0000

    created shopui and injested data

---
+++ old_exercises/school2.py
@@ -0,0 +1,64 @@
+#!/var/www/test/.venv/bin/cgi_venv_launch
+
+import os
+import mysql.connector as database
+
+username = os.environ.get("DB_USERNAME")
+password = os.environ.get("DB_PASSWORD")
+
+conn = database.connect(
+    user=username,
+    password=password,
+    host="localhost",
+    database="school"
+)
+cursor = conn.cursor()
+
+def add_student(first_name: str, last_name: str) -> None:
+    try:
+        statement = "INSERT INTO students (first_name,last_name) VALUES (%s, %s)"
+        data = (first_name, last_name)
+        cursor.execute(statement, data)
+        conn.commit()
+        print("Successfully added student to database")
+    except database.Error as e:
+        print(f"Error adding student to database: {e}")
+
+
+def print_student_data(last_name: str) -> None:
+    try:
+        statement = "SELECT first_name, last_name FROM students WHERE last_name=%s"
+        data = (last_name,)
+        cursor.execute(statement, data)
+        for first_name, last_name in cursor:
+            print(f"Successfully retrieved student: {first_name}, {last_name}")
+    except database.Error as e:
+        print(f"Error retrieving entry from database: {e}")
+
+
+def print_student_data_except(exclude_last_name: str) -> None:
+    try:
+        statement = "SELECT first_name, last_name FROM students WHERE last_name!=%s"
+        data = (exclude_last_name,)
+        cursor.execute(statement, data)
+        for first_name, last_name in cursor:
+            print(f"Successfully retrieved student: {first_name}, {last_name}")
+    except database.Error as e:
+        print(f"Error retrieving entry from database: {e}")
+
+
+add_student("Luca", "Bombardelli")
+add_student("Luca", "Brecher")
+add_student("Berke", "Erdik")
+add_student("Marcel", "Hager")
+add_student("Luka", "Jandrić")
+add_student("Tobias", "Krismer")
+add_student("Dominik", "Lechner")
+add_student("Emanuel", "Mair")
+add_student("Leon", "Pezzei")
+add_student("Thomas", "Schmid")
+add_student("Lucas", "Tusch")
+print_student_data_except("Doe")
+
+
+conn.close()
